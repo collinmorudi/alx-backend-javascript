97cf79886f0378d5afb729e9cd662196a72f594b
Collin Morudi <collinmorudi@gmail.com>
2024-10-03 14:10:24 +0200

    Add: 0-classroom.js solution

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+// 0-classroom.js
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
