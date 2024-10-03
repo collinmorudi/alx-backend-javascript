3722f505fe5b8a309e9c116f2fd7c5d8c2af9cb2
Collin Morudi <collinmorudi@gmail.com>
2024-10-03 14:40:42 +0200

    Add: 7-airport.js solution

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Override the toString method to return the airport code
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
