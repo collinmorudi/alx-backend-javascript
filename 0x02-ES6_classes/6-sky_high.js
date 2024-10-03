658df29887bc141c66576ce175b16cd66c36dbd3
Collin Morudi <collinmorudi@gmail.com>
2024-10-03 14:37:55 +0200

    Add: 6-sky_high.js solution

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+// eslint-disable-next-line import/extensions
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    // Call the parent class constructor to set the sqft attribute
+    super(sqft);
+    this.floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Setter for floors
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
