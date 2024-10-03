38253682ab133aed6e765181beff9414acd1ca31
Collin Morudi <collinmorudi@gmail.com>
2024-10-03 15:26:04 +0200

    Add: 10-car solution

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this[_brand], this[_motor], this[_color]);
+  }
+
+  // Optional: Getters to access the properties if needed
+  get brand() {
+    return this[_brand];
+  }
+
+  get motor() {
+    return this[_motor];
+  }
+
+  get color() {
+    return this[_color];
+  }
+}
