086e9c189615ea709edebf12705e12aa7f37dc80
Collin Morudi <collinmorudi@gmail.com>
2024-10-04 15:35:35 +0200

    Fixed te erro on the bonus task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,17 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable quotes */
+import { uploadPhoto, createUser } from './utils';
+
 export default async function asyncUploadUser() {
   try {
-    // eslint-disable-next-line no-undef
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     return {
       photo,
       user,
     };
   } catch (error) {
+    console.error("Error during async operations:", error); // Debug log to see the error
     return {
       photo: null,
       user: null,
