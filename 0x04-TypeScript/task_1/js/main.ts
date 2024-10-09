03890fa322e04510640cdfbe805c3da5a2d0ede6
Collin Morudi <collinmorudi@gmail.com>
2024-10-09 12:15:00 +0200

    Update: extend teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,19 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Extend the Teacher interface to create Directors
+interface Directors extends Teacher {
+    numberOfReports: number; // This is a mandatory attribute for Directors
+}
+
+// Example usage of Directors
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17
+};
+
+console.log(director1);
