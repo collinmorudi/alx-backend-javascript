b1e14ade0a4f0fc496673b81a0ebc3505f00d0e3
Collin Morudi <collinmorudi@gmail.com>
2024-10-09 12:10:20 +0200

    Add: task_1 solution

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+// Teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+// Example usage
+const teacher3: Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false
+};
+
+console.log(teacher3);
