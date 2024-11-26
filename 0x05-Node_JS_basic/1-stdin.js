922df46e15efdd99e4cc137e8699727e7d7a0ca6
Collin Morudi <collinmorudi@gmail.com>
2024-11-26 23:10:42 +0200

    fixed the closing message

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,11 +7,12 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const isPiped = !process.stdin.isTTY;
+
 rl.question('Welcome to Holberton School, what is your name? \n', (name) => {
   console.log(`Your name is: ${name}`);
+  if (isPiped) {
+    console.log('This important software is now closing\n');
+  }
   rl.close();
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing\n');
 });
